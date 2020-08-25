5550f8615032645a389e7c9fb16a80c670b7e1e1
SWFullPotential <swfullpotential@gmail.com>
2020-08-25 16:33:32 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+
+
+document.querySelector("main#main").remove()
+
+let newHeader = document.createElement('h1')
+document.body.appendChild(newHeader)
+newHeader.id = "victory"
+newHeader.innerHTML = "<h1>Samantha is the champion!</h1>";
+// debugger
+
