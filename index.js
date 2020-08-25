b23b2597431fbbcfc1255899d8c64d2868a6fb2f
francepants <franceline90@gmail.com>
2020-08-25 14:14:25 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+
+// AssertionError: Make sure you remove the <main> with id 'main': expected <main id="main"></main> to not exist
+
+let main = document.querySelector('main')
+main.remove()
+
+let newHeader = document.createElement('h1')
+newHeader.id = "victory"
+newHeader.innerHTML = "I is the champion"
