16ccebfd83da88158e7339ca5c8c7fd561696287
Eyeruz <3yerus@gmail.com>
2020-08-26 21:06:28 -0400

    Done.

--- index.js
+++ index.js
@@ -2,4 +2,7 @@
 
 main.remove()
 
-let newHeader = document.querySelector("h1#victory");
+let newHeader = document.createElement("h1");
+
+newHeader.id = "victory"
+newHeader.innerHTML = "Eyerus is the champion"
