706647d09dbcdb099e25fb4026c27a66e4d21040
tonyhuynh6789 <duc_thinh_03@yahoo.com>
2020-09-01 14:26:15 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+const remove = main.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.classname = 'victory';
+newHeader.innerHTML = 'Tony Rim is the champion';
+newHeader.id = 'victory'
+
+
